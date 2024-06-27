a8a5fbab44bb15058562bc512c6973e8ce4e9f37
Jesus <jesus.salazar.v35@gmail.com>
2024-06-27 12:31:42 -0600

    Se agrego una tabla para mostrar el subMenu activo y se le agregaron las alertas al actualizar

--- src/Components_Panel/FormSeleccionAreas.jsx
+++ src/Components_Panel/FormSeleccionAreas.jsx
@@ -1,8 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { useInsoel } from '../Context/InsoelContext';
+import React, { useState, useEffect } from "react";
+import { useInsoel } from "../Context/InsoelContext";
 
-function FormSeleccionAreas() {
-  const { areas, obtenerAreas, getProyectosByArea, updateSubMenu, obtenerSubMenu, idSubMenu } = useInsoel();
+function FormSeleccionAreas({ reloadSubMenu, toast }) {
+  const {
+    areas,
+    obtenerAreas,
+    getProyectosByArea,
+    updateSubMenu,
+    obtenerSubMenu,
+    idSubMenu,
+  } = useInsoel();
 
   useEffect(() => {
     obtenerAreas();
@@ -21,7 +28,7 @@ function FormSeleccionAreas() {
   }, [selecciones]);
 
   const handleSeleccion = async (area) => {
-    if (areasSeleccionadas.some(a => a._id === area._id)) {
+    if (areasSeleccionadas.some((a) => a._id === area._id)) {
       const nuevasAreas = areasSeleccionadas.filter((a) => a._id !== area._id);
       setAreasSeleccionadas(nuevasAreas);
       const nuevosProyectos = { ...proyectosPorArea };
@@ -60,30 +67,36 @@ function FormSeleccionAreas() {
     });
   };
 
-  const handleGuardarSelecciones = () => {
+  const handleGuardarSelecciones = async () => {
     const subMenu = {
-      "area1" : areaIds[0],
-      "area2" : areaIds[1],
-      "area3" : areaIds[2],
-      "area4" : areaIds[3],
-      "enlace1" : proyectoIds[0],
-      "enlace2" : proyectoIds[1],
-      "enlace3" : proyectoIds[2],
-      "enlace4" : proyectoIds[3],
-    }
-    updateSubMenu(idSubMenu, subMenu)
-    console.log(idSubMenu)
+      area1: areaIds[0],
+      area2: areaIds[1],
+      area3: areaIds[2],
+      area4: areaIds[3],
+      enlace1: proyectoIds[0],
+      enlace2: proyectoIds[1],
+      enlace3: proyectoIds[2],
+      enlace4: proyectoIds[3],
+    };
+    await toast.promise(updateSubMenu(idSubMenu, subMenu), {
+      pending: "Actualizando Sub Menu",
+      success: "Sub Menu actualizado con Éxito",
+      error: "Error al actualizar el sub menu",
+    });
+    reloadSubMenu()
   };
 
   return (
-    <div className='ml-10 mr-10 mt-10'>
+    <div className="ml-10 mr-10 mt-10">
       <p>Selecciona hasta cuatro áreas:</p>
       <div className="flex flex-wrap">
         {areas.map((area, index) => (
           <button
             key={index}
             className={`py-2 px-4 m-2 rounded-lg ${
-              areasSeleccionadas.some(a => a._id === area._id) ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'
+              areasSeleccionadas.some((a) => a._id === area._id)
+                ? "bg-blue-500 text-white"
+                : "bg-gray-200 text-gray-700"
             }`}
             onClick={() => handleSeleccion(area)}
           >
@@ -99,8 +112,10 @@ function FormSeleccionAreas() {
               <span className="mr-2">{area.area}</span>
               <select
                 className="ml-2 p-1 border border-gray-400 rounded"
-                value={selecciones[area._id] || ''}
-                onChange={(e) => handleSeleccionOpcion(area._id, e.target.value)}
+                value={selecciones[area._id] || ""}
+                onChange={(e) =>
+                  handleSeleccionOpcion(area._id, e.target.value)
+                }
               >
                 <option value="">Selecciona una opción</option>
                 {proyectosPorArea[area._id] &&
@@ -114,7 +129,10 @@ function FormSeleccionAreas() {
           ))}
         </ul>
       </div>
-      <button onClick={handleGuardarSelecciones} className="bg-green-500 text-white px-4 py-2 rounded mt-4">
+      <button
+        onClick={handleGuardarSelecciones}
+        className="bg-green-500 text-white px-4 py-2 rounded mt-4"
+      >
         Guardar selecciones
       </button>
     </div>
--- src/Pages_panel/SubMenuPage.jsx
+++ src/Pages_panel/SubMenuPage.jsx
@@ -1,29 +1,67 @@
-import React, { useState } from "react";
-import FormSeleccionAreas from '../Components_Panel/FormSeleccionAreas'
+import React, { useEffect, useState } from "react";
+import FormSeleccionAreas from "../Components_Panel/FormSeleccionAreas";
+import { useInsoel } from "../Context/InsoelContext";
+import SubMenuTable from "../Components_Panel/subMenuTable";
+import {toast, ToastContainer} from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css"
 
 function SubMenuPage() {
-  const [mostrarFormulario, setMostrarFormulario] = useState(false)
-  const handleClickActualizarSubMenu = () =>{
-    setMostrarFormulario(true)
+  const [mostrarFormulario, setMostrarFormulario] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [recargar, setRecargar] = useState(false);
+
+  const { obtenerSubMenu, subMenu } = useInsoel();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await obtenerSubMenu();
+      setIsLoading(false);
+    };
+
+    fetchData();
+    setRecargar(false)
+  }, [recargar]);
+
+  const handleReloadSubMenu = () =>{
+    setRecargar(true)
+    setMostrarFormulario(false)
   }
+
+  const handleClickActualizarSubMenu = () => {
+    setMostrarFormulario(true);
+  };
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="flex justify-between items-center mb-8 ">
         {!mostrarFormulario && (
-        <h1 className="mt-16 text-3xl font-bold ml-5">Sub Menu </h1>
-      )}
-      {
-        mostrarFormulario ? (
+          <h1 className="mt-16 text-3xl font-bold ml-5">Sub Menu </h1>
+        )}
+        {mostrarFormulario ? (
           <div>
-            <FormSeleccionAreas/>
+            <FormSeleccionAreas reloadSubMenu = {handleReloadSubMenu} toast={toast} />
           </div>
-        ):(
-          <button onClick={handleClickActualizarSubMenu} className="mt-14 bg-tertiary hover:bg-secondary text-white py-2 px-4 rounded-md">
+        ) : (
+          <button
+            onClick={handleClickActualizarSubMenu}
+            className="mt-14 bg-tertiary hover:bg-secondary text-white py-2 px-4 rounded-md"
+          >
             Actualizar SubMenu
           </button>
-        )
-      }
+          
+        )}
       </div>
+
+      <ToastContainer/>
+
+      {!mostrarFormulario && (
+        isLoading ? (
+          <div className="text-center">
+            <p className="text-xl">Cargando...</p>
+          </div>
+        ) : (
+          <SubMenuTable subMenu={subMenu} />
+        )
+      )}
     </div>
   );
 }
